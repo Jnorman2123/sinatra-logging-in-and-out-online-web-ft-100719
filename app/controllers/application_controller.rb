c2d0a771a5863101556fb5b25db160cdce88ddf4
Jonathan Norman <jnorman2123@gmail.com>
2019-11-22 17:31:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     # binding.pry
-    @user = User.find(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
     if @user
       erb :account
 
