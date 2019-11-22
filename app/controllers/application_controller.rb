7c6bc46ad443f8a0151063325a6e9188daa2cff0
Jonathan Norman <jnorman2123@gmail.com>
2019-11-22 17:19:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,9 +23,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    @user = User.find_by_username(params[:username])
+    if @user
       erb :account
 
+    else
+      erb :error
+    end
 
   end
 
